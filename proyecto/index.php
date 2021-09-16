ac974d2277ba15e27ca13cabb87b3c155d361595
juan3352 <al033523@uacam.mx>
2021-09-16 16:19:51 -0500

    actualizacion

---
+++ proyecto/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mi blog</title>
+        <link rel="stylesheet" href="estilos/estilos.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    </head>
+    <body class="principal">
+        <?php
+        ?>    
+        <div>
+            <header class="cabeza">
+                <div class="container">
+                    <h1 style="color: white;">Mi blog personal</h1>
+
+                    <div class="nav-wrapper">
+                        <nav style="background-color:rgba(255,0,0,0)">
+                            <a href="index.php" class="brand-logo">Juan Domingo Cuc Huchin</a>
+                            <ul class="right hide-on-med-and-down">
+                                <li><a href="index.php" style="color:aqua;">Principal</a></li>
+                                <li><a href="general.php" style="color:aqua;">Datos generales</a></li>
+                                <li><a href="metas.php" style="color:aqua;">Metas</a></li>
+
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+            <hr>
+
+
+            <div>
+                <div class="row">
+                    <div class="col s12">
+                        <p class="parrafo">Hola, soy estudiante de ingeniería en sistemas computacionales, de la facultad de ingeniería de la universidad autónoma de campeche.</p>
+                        <div class="col s6"> <img src="imagenes/fiuac.png" width="600" alt="no se encontro la imagen" title="auto prueba"></div>
+                        <div class="col s6">
+                            <p class="parrafo">La facultad de ingeniería es uno de los 5 campus de la universidad autónoma de campeche</p>
+                            <p class="parrafo">El plan de estudios de la Licenciatura en Ingeniero en Sistemas Computacionales, es Flexible, por competencias, centrado en el aprendizaje y se propone una estructura ideal en 8 semestres, con de 62 unidades de aprendizaje. La
+                                carga académica es regulada por el sistema de créditos SATCA, que tiene un total de 230 créditos académicos que el alumno puede cubrir en un plazo mínimo de 7 semestres y hasta 12 semestres. La secuencia de unidades de aprendizaje
+                                pretende una transmisión ordenada de la información al estudiante, la generación paulatina de capacidades analíticas y metodológicas, y la oportunidad para que el estudiante genere su propia síntesis de lo aprendido, considerando
+                                el Modelo Educativo de la Universidad 2009. En forma extracurricular el alumno debe acreditar las competencias en Microsoft: Word, Excel y Power Point. En las etapas avanzadas del programa, los estudiantes podrán escoger para
+                                su formación un poco más especializada, las optativas ofertadas de: Ingeniería en Software, Sistemas Integrales y Redes Deberá cumplir con el Servicio Social de acuerdo con lo establecido en la Legislación Universitaria.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+        <footer class="page-footer" style="background-color: black;">
+            <div class="foot1">
+                <div class="container">
+                    <div class="row">
+                        <div class="col l6 s20">
+                            <h5 class="white-text">Mi blog personal</h5>
+                            <p class="grey-text text-lighten-4">Esta pagina web esta creada solo con fines educativos, la informacion que se muestra podria no ser real, al no ser una pagina con fin economico, todas las imagenes reservan los derechos de autor de cada una.</p>
+                        </div>
+                        <div class="col l4 offset-l2 s12">
+                            <h5 class="white-text">Links de interés</h5>
+                            <ul>
+                                <li><a class="grey-text text-lighten-3" href="https://www.uacam.mx/" target="_blank">UAC</a></li>
+                                <li><a class="grey-text text-lighten-3" href="http://fi.uacam.mx/" target="_blank">Facultad de Ingeniería</a></li>
+                                <li><a class="grey-text text-lighten-3" href="#!" target="_blank">ff</a></li>
+                                <li><a class="grey-text text-lighten-3" href="#!" target="_blank">Link 4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                    <div class="container">
+                        © Juan Domingo Cuc Huchin © 2021 by JDCH
+
+                    </div>
+                </div>
+            </div>
+        </footer> 
+
+    </body>
+</html>
